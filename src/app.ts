e2e0549fbc90fe6fc102cd1da3fe615cb439b5ba
Krzysztof Brzoska <krzysztof.brzoska@hawk-e.com.pl>
2025-01-28 13:05:34 +0100

    useful api packages added

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
 import 'dotenv/config'
 import routes from 'routes'
 
 const app = express()
 const MONGO_URI = process.env.MONGO_URI as string
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
 
 mongoose.connect(MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
