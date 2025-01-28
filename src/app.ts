e6cc5f8c77af62b37635394c21e73b3364bc5802
Krzysztof Brzoska <krzysztof.brzoska@hawk-e.com.pl>
2025-01-28 12:50:20 +0100

    connection to mongodb

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,9 @@ import mongoose from 'mongoose';
 import 'dotenv/config'
 
 const app = express()
+const MONGO_URI = process.env.MONGO_URI as string
 
-const {MONGO_URI} = process.env
-
-mongoose.connect(MONGO_URI, { useNewUrlParser: true })
+mongoose.connect(MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
